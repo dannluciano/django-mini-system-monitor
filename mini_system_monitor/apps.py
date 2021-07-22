977feec09b7c64a03f4d6edd0cc39fab61f29f43
Dann Luciano <dannluciano@gmail.com>
2021-07-22 19:53:28 -0300

    Fix Hint DEFAULT_AUTO_FIELD

--- mini_system_monitor/apps.py
+++ mini_system_monitor/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class MiniSystemMonitorConfig(AppConfig):
     name = 'mini_system_monitor'
     verbose_name = 'System Monitor'
+
+    default_auto_field = 'django.db.models.AutoField'
